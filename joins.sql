9d5ebd3ec2ce1dbb9da19859de7921b1fda4b499
ktakesue <ktakesue@aol.com>
2018-04-03 22:39:16 -1000

    fixed the joins for accurate results

--- joins.sql
+++ joins.sql
@@ -24,32 +24,32 @@ WHERE posts.created_at > '2015-01-01';
 
 --Create a query to get the all rows in the comments table, 
 --showing post title (aliased as 'Post Title'), and the all the comment's fields
-SELECT comments.*, posts.title AS 'Post Title' FROM comments INNER JOIN posts ON comments.user_id = posts.user_id;
+SELECT comments.*, posts.title AS post_title FROM comments INNER JOIN posts ON comments.post_id = posts.post_id;
 
 --Create a query to get the all rows in the comments table, 
 --showing post title (aliased as post_title), post url (ailased as post_url), 
 --and the comment body (aliased as comment_body) where the post was created before January 1, 2015
 SELECT comments.*, posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body FROM comments 
-INNER JOIN posts ON comments.user_id = posts.user_id WHERE posts.created_at < '2015-01-01';
+INNER JOIN posts ON comments.post_id = posts.post_id WHERE posts.created_at < '2015-01-01';
 
 --Create a query to get the all rows in the comments table, 
 --showing post title (aliased as post_title), post url (ailased as post_url), 
 --and the comment body (aliased as comment_body) where the post was created after January 1, 2015
 SELECT comments.*, posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body FROM comments 
-INNER JOIN posts ON comments.user_id = posts.user_id WHERE posts.created_at > '2015-01-01';
+INNER JOIN posts ON comments.post_id = posts.post_id WHERE posts.created_at > '2015-01-01';
 
 --Create a query to get the all rows in the comments table, 
 --showing post title (aliased as post_title), post url (ailased as post_url), 
 --and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
 SELECT comments.*, posts.title AS post_title, posts.url AS post_url, comments.body AS comment_body FROM comments 
-INNER JOIN posts ON comments.user_id = posts.user_id WHERE comments.body LIKE '%USB%';
+INNER JOIN posts ON comments.post_id = posts.post_id WHERE comments.body LIKE '%USB%';
 
 --Create a query to get the post title (aliased as post_title), first name of the author of the post, 
 --last name of the author of the post, and comment body (aliased to comment_body), 
 --where the comment body contains the word 'matrix' ( should have 855 results )
 SELECT posts.title AS post_title, users.first_name, users.last_name, comments.body AS comment_body FROM comments 
 INNER JOIN users ON comments.user_id = users.user_id 
-INNER JOIN posts ON comments.user_id = posts.user_id 
+INNER JOIN posts ON comments.post_id = posts.post_id 
 WHERE comments.body LIKE '%matrix%';
 
 --Create a query to get the first name of the author of the comment, last name of the author of the comment, 
@@ -57,7 +57,7 @@ WHERE comments.body LIKE '%matrix%';
 --and the post content contains the word 'dolorum' ( should have 102 results )
 SELECT users.first_name, users.last_name, comments.body AS comment_body FROM comments
 INNER JOIN users ON comments.user_id = users.user_id 
-INNER JOIN posts ON comments.user_id = posts.user_id
+INNER JOIN posts ON comments.post_id = posts.post_id
 WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
 
 --Create a query to get the first name of the author of the post (aliased to post_author_first_name), 
@@ -68,8 +68,8 @@ WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
 SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name, 
 posts.title AS post_title, users.username AS comment_author_username, comments.body AS comment_body
 FROM comments INNER JOIN users ON comments.user_id = users.user_id 
-INNER JOIN posts ON comments.user_id = posts.user_id
-WHERE comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%' AND posts.content LIKE '%nemo%';
+INNER JOIN posts ON comments.post_id = posts.post_id
+WHERE (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%') AND posts.content LIKE '%nemo%';
 
 --Count how many comments have been written on posts that have been created after July 14, 2015 
 --( should have one result, the value of the count should be 27)
